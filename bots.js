26fe0e467a5bf835fe63c044c7428b6323c21077
KAPIL BINDAL <kapilbindal1@gmail.com>
2022-01-27 23:09:05 +0530

    Update bots.js

--- bots.js
+++ bots.js
@@ -2,12 +2,12 @@ const { sell1 } = require("./market/sell");
 const { buy1, buy2 } = require("./market/buy");
 const bots = [
   {
-    name: "test_089",
+    name: "test_89",
     buyFunction: buy1.buy,
     sellFunction: sell1.sell,
   },
   {
-    name: "test_90",
+    name: "yash_bhuvan",
     buyFunction: buy2.buy,
     sellFunction: sell1.sell,
     indicatorFunction: buy2.buy
