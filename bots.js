ed2a8b41241c5153b9ace0d48d42676802233a72
KAPIL BINDAL <kapilbindal1@gmail.com>
2022-01-27 19:01:18 +0530

    sell logic fix

--- bots.js
+++ bots.js
@@ -2,7 +2,7 @@ const { sell1 } = require("./market/sell");
 const { buy1 } = require("./market/buy");
 const bots = [
   {
-    name: "test12345",
+    name: "test_89",
     buyFunction: buy1.buy,
     sellFunction: sell1.sell,
   }
