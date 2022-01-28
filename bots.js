e2d72c42245d17cbba38a33f3e320806194b3f47
adriantelles <adrian.telles@walmart.com>
2022-01-28 17:00:40 +0530

    Update bots.js

--- bots.js
+++ bots.js
@@ -2,19 +2,9 @@ const { sell1 } = require("./market/sell");
 const { buy1, buy2 } = require("./market/buy");
 const bots = [
   {
-    name: "test_89",
-    buyFunction: buy1.buy,
-    sellFunction: sell1.sell,
-  },
-  {
-    name: "test_99",
-    buyFunction: buy1.buy,
-    sellFunction: sell1.sell,
-  },
-  {
-    name: "yash_bhuvan_EMA_singleBuy",
+    name: "yash_bhuvan_EMA",
     buyFunction: buy2.buy,
-    sellFunction: sell1.sell,
+    // sellFunction: sell1.sell,
     indicatorFunction: buy2.indicator,
   },
 ];
