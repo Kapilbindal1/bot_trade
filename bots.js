2958552dddeab6163a768986273818ce7b8fc419
adriantelles <adrian.telles@walmart.com>
2022-02-03 10:25:32 +0530

    rsi bot added

--- bots.js
+++ bots.js
@@ -2,6 +2,11 @@ const { buy2 } = require("./market/buy");
 
 const bots = [
   {
+    name: "test_201",
+    buyFunction: buy1.buy,
+    sellFunction: sell1.sell,
+  },
+  {
     name: "Yash_Bhuvan_SOL",
     buyFunction: buy2.buy,
     indicatorFunction: buy2.indicator,
