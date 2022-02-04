91f181c03f4f4c427187dfc7b8a3c1ca8c8455ae
Kapil Bindal <kapilbindal1@gmail.com>
2022-02-04 15:04:35 +0530

    bots updated fida coin

--- bots.js
+++ bots.js
@@ -3,7 +3,12 @@ const { sell1 } = require("./market/sell");
 
 const bots = [
   {
-    name: "THREE_INDICATORS(with loggers)",
+    name: "test_301",
+    buyFunction: buy1.buy,
+    sellFunction: sell1.sell,
+  },
+  {
+    name: "THREE_INDICATORS_FIDA",
     buyFunction: buy3.buy,
     indicatorFunction: buy3.indicator,
   },
