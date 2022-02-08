f60b957d301834fbf6e1ae1d75cc0b4a4ecd4dd9
KAPIL BINDAL <kapilbindal1@gmail.com>
2022-02-08 16:05:53 +0530

    minor fix

--- bots.js
+++ bots.js
@@ -2,21 +2,10 @@ const { sell1 } = require("./market/sell");
 const { buy1, buy2 } = require("./market/buy");
 const bots = [
   {
-    name: "test_89",
+    name: "test_1",
     buyFunction: buy1.buy,
     sellFunction: sell1.sell,
-  },
-  {
-    name: "test_99",
-    buyFunction: buy1.buy,
-    sellFunction: sell1.sell,
-  },
-  {
-    name: "yash_bhuvan_EMA_singleBuy",
-    buyFunction: buy2.buy,
-    sellFunction: sell1.sell,
-    indicatorFunction: buy2.indicator,
-  },
+  }
 ];
 
 module.exports = { bots };
