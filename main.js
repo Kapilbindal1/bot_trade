136d8478d1cd99d1dd69d72e0ea07ba6f995c679
Kapil Bindal <kapil.bindal@emergemarket.com>
2022-02-25 11:56:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -277,6 +277,6 @@ const main = async () => {
 const port = process.env.PORT || 8000;
 app.listen(port, function () {
   console.log("App listening at: ", port);
-  main();
+  // main();
   keepAlive();
 });
