5050eefd7a2a197405f43338b885d8e15b0c04dc
Kapil Bindal <kapil.bindal@emergemarket.com>
2022-05-23 10:52:37 +0530

    minor fix

--- bots.js
+++ bots.js
@@ -3,10 +3,22 @@ const { sell1 } = require("./market/sell");
 
 const bots = [
   {
-    name: "matic_usdt_local",
+    name: "matic_usdt",
     buyFunction: buy1.buy,
     sellFunction: sell1.sell,
     config: { asset: "MATIC", base: "USDT" }
+  },
+  {
+    name: "ens_usdt",
+    buyFunction: buy1.buy,
+    sellFunction: sell1.sell,
+    config: { asset: "ENS", base: "USDT" }
+  },
+  {
+    name: "eth_usdt",
+    buyFunction: buy1.buy,
+    sellFunction: sell1.sell,
+    config: { asset: "ETH", base: "USDT" }
   }
 ];
 
