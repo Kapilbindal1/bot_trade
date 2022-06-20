468671d8a2ae7946e84ee83f7f2e6fb25e4c3687
Kapil <kapilbindal1@gmail.com>
2022-06-20 11:14:33 +0530

    markets currencies

--- technicalIndicatorsApi/controller.js
+++ technicalIndicatorsApi/controller.js
@@ -5,9 +5,28 @@ var TechnicalIndicators = require("../db/schemas/technical_Indicators");
 exports.getTechnicalIndicators = async (req, res) => {
   try {
     const techIndicators = await TechnicalIndicators.find({});
-    console.log(techIndicators, "techIndicators1234");
     res.send({ success: true, data: techIndicators });
   } catch (err) {
     return { success: false, err: err };
   }
 };
+
+exports.getMarketCurrencies = async (req, res) => {
+  let binance = new ccxt.binance();
+  let marketPrices = await binance.fetchTickers();
+  res.send({ success: true, data: marketPrices });
+};
+
+exports.getMarketPrices = async (req, res) => {
+  while (true) {
+    try {
+      // let ticker = await exchange.fetchTicker();
+      // console.log(ticker, 'ticker');
+      // res.send({ success: true, data: ticker });
+    }
+    catch (e) {
+      console.log(e);
+      break;
+    }
+  }
+}
