82ad03ad805fe3ad45dfde24a49f81215158c5f7
Harshit <devinnow8@gmail.com>
2023-04-05 18:26:48 +0530

    Update alive.js

--- alive.js
+++ alive.js
@@ -3,21 +3,21 @@ const dotEnv = require("dotenv");
 
 const keepAlive = () => {
   dotEnv.config();
-  setInterval(() => {
-    axios
-      .get(`${process.env.APP_URL}test`)
-      .then(function (response) {
-        // handle success
-        console.log(response);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-      .then(function () {
-        // always executed
-      });
-  }, 5 * 60 * 1000);
+  // setInterval(() => {
+  //   axios
+  //     .get(`${process.env.APP_URL}test`)
+  //     .then(function (response) {
+  //       // handle success
+  //       console.log(response);
+  //     })
+  //     .catch(function (error) {
+  //       // handle error
+  //       console.log(error);
+  //     })
+  //     .then(function () {
+  //       // always executed
+  //     });
+  // }, 5 * 60 * 1000);
 };
 
 module.exports = { keepAlive };
