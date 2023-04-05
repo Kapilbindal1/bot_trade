70f9561691773fde675f6f097d1765e85cde4125
Bhuvan Garg <kapilbindal1@gmail.com>
2023-04-05 13:00:40 +0530

    db schema changes for bots

--- db/schemas/bots.js
+++ db/schemas/bots.js
@@ -3,6 +3,9 @@ const schema = mongoose.Schema;
 
 const botsSchema = new schema({
   name: { type: String, required: true, unique: true },
+  img:{type: String, },
+  infoData:{type: String,},
+  alert:{type: String, },
   date: { type: Date, default: Date.now },
 });
 
