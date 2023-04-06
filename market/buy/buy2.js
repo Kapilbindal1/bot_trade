029b1f4a025291063cd68b4dbdd7d373869bd783
Harshit <devinnow8@gmail.com>
2023-04-06 13:03:28 +0530

    make live on render

--- market/buy/buy2.js
+++ market/buy/buy2.js
@@ -1,5 +1,5 @@
 /**
- * It's a combination of RSI and MACD for 5 min interval
+ * It's a combination of EMA for 5 min interval
  */
 
 const Market = require("../index");
@@ -8,28 +8,15 @@ const { EMA } = require("../indicators");
 const config = require("../../constants/config");
 
 const indicator = async ({ balance, currentPrice, asset }) => {
-  if (balance < config.minimumBuy)
-    return { quantity: 0, message: "less than minimum balance" };
-    
+  if (balance < config.minimumBuy) return { quantity: 0, message: "less than minimum balance" };
+
   let historicalData_5m = await Market.getHistoricalData();
   let indicatorInputData_5m = MainUtils.getCloseInputData(historicalData_5m);
 
   let EMA_result_5m_9 = EMA.calculateEMAValue(indicatorInputData_5m, 9);
   let EMA_result_5m_18 = EMA.calculateEMAValue(indicatorInputData_5m, 18);
-  const prevAdviceEMA = EMA.getAdvice(
-    EMA_result_5m_9[EMA_result_5m_9.length - 2],
-    EMA_result_5m_18[EMA_result_5m_18.length - 2],
-    currentPrice,
-    null,
-    asset
-  );
-  const adviceEMA = EMA.getAdvice(
-    EMA_result_5m_9[EMA_result_5m_9.length - 1],
-    EMA_result_5m_18[EMA_result_5m_18.length - 1],
-    currentPrice,
-    prevAdviceEMA.advice,
-    asset
-  );
+  const prevAdviceEMA = EMA.getAdvice(EMA_result_5m_9[EMA_result_5m_9.length - 2], EMA_result_5m_18[EMA_result_5m_18.length - 2], currentPrice, null, asset);
+  const adviceEMA = EMA.getAdvice(EMA_result_5m_9[EMA_result_5m_9.length - 1], EMA_result_5m_18[EMA_result_5m_18.length - 1], currentPrice, prevAdviceEMA.advice, asset);
 
   return adviceEMA;
 };
