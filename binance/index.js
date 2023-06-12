5207389ddf50d02b96a2d05c6860ef964e9dbd49
Kapil Bindal <kapil.bindal@emergemarket.com>
2023-06-13 00:20:37 +0530

    Update index.js

--- binance/index.js
+++ binance/index.js
@@ -4,13 +4,18 @@ const ccxt = require("ccxt");
 let client;
 
 const getClient = () => {
+  console.log("client: ", client);
   if (client) return client;
   dotEnv.config();
+
+  console.log("process.env.API_KEY: ", process.env.API_KEY);
 
   client = new ccxt.binance({
     apiKey: process.env.API_KEY,
     secret: process.env.API_SECRET
   });
+
+  console.log("client1: ", client);
 
   return client;
 };
