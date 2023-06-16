8e3fecf613aa8c4c09ec9423b40902ca5f026e95
Kapil Bindal <kapil.bindal@emergemarket.com>
2023-06-16 15:52:29 +0530

    Update bots.js

--- bots.js
+++ bots.js
@@ -2,18 +2,6 @@ const { buy1, buy3, buyROC } = require("./market/buy");
 const { sell1 } = require("./market/sell");
 
 const bots = [
-  // {
-  //   name: "matic_usdt",
-  //   buyFunction: buy1.buy,
-  //   sellFunction: sell1.sell,
-  //   config: { asset: "MATIC", base: "USDT" }
-  // },
-  // {
-  //   name: "ens_usdt",
-  //   buyFunction: buy1.buy,
-  //   sellFunction: sell1.sell,
-  //   config: { asset: "ENS", base: "USDT" }
-  // },
   {
     name: "eth_usdt",
     buyFunction: buy1.buy,
@@ -38,66 +26,53 @@ const bots = [
     sellFunction: sell1.sell,
     config: { asset: "MATIC", base: "USDT", market: 'MATIC/USDT', buyLot: 20 }
   },
-  // {
-  //   name: "matic_usdt_30m",
-  //   buyFunction: buy1.buy_30m,
-  //   sellFunction: sell1.sell,
-  //   config: { asset: "MATIC", base: "USDT" }
-  // },
-  // {
-  //   name: "ens_usdt_30m",
-  //   buyFunction: buy1.buy_30m,
-  //   sellFunction: sell1.sell,
-  //   config: { asset: "ENS", base: "USDT" }
-  // },
-  // {
-  //   name: "eth_usdt_30m",
-  //   buyFunction: buy1.buy_30m,
-  //   sellFunction: sell1.sell,
-  //   config: { asset: "ETH", base: "USDT" }
-  // },
-  // {
-  //   name: "matic_usdt_instant",
-  //   buyFunction: buy1.buy_InstantDown,
-  //   sellFunction: sell1.sell,
-  //   config: { asset: "MATIC", base: "USDT" }
-  // },
-  // {
-  //   name: "ens_usdt_instant",
-  //   buyFunction: buy1.buy_InstantDown,
-  //   sellFunction: sell1.sell,
-  //   config: { asset: "ENS", base: "USDT" }
-  // },
-  // {
-  //   name: "eth_usdt_instant",
-  //   buyFunction: buy1.buy_InstantDown,
-  //   sellFunction: sell1.sell,
-  //   config: { asset: "ETH", base: "USDT" }
-  // },
-  // {
-  //   name: "bean_usdt_instant",
-  //   buyFunction: buy1.buy_InstantDown,
-  //   sellFunction: sell1.sell,
-  //   config: { asset: "BEAM", base: "USDT" }
-  // },
-  // {
-  //   name: "fida_usdt_instant",
-  //   buyFunction: buy1.buy_InstantDown,
-  //   sellFunction: sell1.sell,
-  //   config: { asset: "FIDA", base: "USDT" }
-  // },
-  // {
-  //   name: "bnb_usdt_instant",
-  //   buyFunction: buy1.buy_InstantDown,
-  //   sellFunction: sell1.sell,
-  //   config: { asset: "BNB", base: "USDT" }
-  // },
-  // {
-  //   name: "sol_usdt_instant",
-  //   buyFunction: buy1.buy_InstantDown,
-  //   sellFunction: sell1.sell,
-  //   config: { asset: "SOL", base: "USDT" }
-  // },
+  {
+    name: "ant_usdt_ROC",
+    buyFunction: buyROC.buy,
+    sellFunction: sell1.sell,
+    config: { asset: "ANT", base: "USDT", market: 'ANT/USDT', buyLot: 20 }
+  },
+  {
+    name: "ant_usdt",
+    buyFunction: buy1.buy,
+    sellFunction: sell1.sell,
+    config: { asset: "ANT", base: "USDT", market: 'ANT/USDT', buyLot: 20 }
+  },
+  {
+    name: "shib_usdt",
+    buyFunction: buy1.buy,
+    sellFunction: sell1.sell,
+    config: { asset: "SHIB", base: "USDT", market: 'SHIB/USDT', buyLot: 20 }
+  },
+  {
+    name: "shib_usdt_ROC",
+    buyFunction: buyROC.buy,
+    sellFunction: sell1.sell,
+    config: { asset: "SHIB", base: "USDT", market: 'SHIB/USDT', buyLot: 20 }
+  }, {
+    name: "doge_usdt_ROC",
+    buyFunction: buyROC.buy,
+    sellFunction: sell1.sell,
+    config: { asset: "DOGE", base: "USDT", market: 'DOGE/USDT', buyLot: 20 }
+  },
+  {
+    name: "doge_usdt",
+    buyFunction: buy1.buy,
+    sellFunction: sell1.sell,
+    config: { asset: "DOGE", base: "USDT", market: 'DOGE/USDT', buyLot: 20 }
+  },
+  {
+    name: "AVAX_usdt_ROC",
+    buyFunction: buyROC.buy,
+    sellFunction: sell1.sell,
+    config: { asset: "AVAX", base: "USDT", market: 'AVAX/USDT', buyLot: 20 }
+  },
+  {
+    name: "COSMOS_usdt_ROC",
+    buyFunction: buyROC.buy,
+    sellFunction: sell1.sell,
+    config: { asset: "ATOM", base: "USDT", market: 'ATOM/USDT', buyLot: 20 }
+  },
 ];
 
 // const bots = [
